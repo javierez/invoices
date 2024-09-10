e833c245dce3ad21f46a68e136882647829d1c6b
Javier Pérez García <javierperezgarcia@Javiers-MacBook-Pro.local>
2024-09-11 00:26:48 +0200

    too soon canté victoria, no funcionaba el fetch data desde el servicio, a ver ahora

--- app/lib/dataGastos.tsx
+++ app/lib/dataGastos.tsx
@@ -1,16 +1,21 @@
 import * as XLSX from 'xlsx';
+import path from 'path';
 
 export async function getExcelDataGastos() {
   if (typeof window === 'undefined') {
     // Server-side
     const { readFile } = await import('fs/promises');
-    const { join } = await import('path');
-    const filePath = join(process.cwd(), 'public', 'data', 'locales-Gastos.xlsx');
-    const fileBuffer = await readFile(filePath);
-    const workbook = XLSX.read(fileBuffer, { type: 'buffer' });
-    const sheetName = workbook.SheetNames[0];
-    const sheet = workbook.Sheets[sheetName];
-    return XLSX.utils.sheet_to_json(sheet);
+    const filePath = path.join(process.cwd(), 'public', 'data', 'locales-Gastos.xlsx');
+    try {
+      const fileBuffer = await readFile(filePath);
+      const workbook = XLSX.read(fileBuffer, { type: 'buffer' });
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
+      return XLSX.utils.sheet_to_json(sheet);
+    } catch (error) {
+      console.error('Error reading Excel file:', error);
+      throw new Error('Unable to read Excel file');
+    }
   } else {
     // Client-side
     const response = await fetch('/data/locales-Gastos.xlsx');
